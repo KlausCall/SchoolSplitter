eb95e544e8231d9fe735e2d11e58b6985cef91aa
KlausCall <75502471+KlausCall@users.noreply.github.com>
2020-12-26 17:10:28 +0100

    make footer float

--- src/modules/Footer.tsx
+++ src/modules/Footer.tsx
@@ -1,7 +1,7 @@
 interface Props {}
 
 const Footer: React.FC<Props> = () => (
-  <footer className="footer p-2 bg-light fixed-bottom text-center">
+  <footer className="footer p-2 bg-light text-center">
     <span className="text-muted" style={{ fontSize: '0.875rem' }}>
       SchoolSplitter {process.env.REACT_APP_VERSION} - (C) 2020 by KlausCall
       &amp; Korki43 - MIT licensed - source hosted on{' '}
--- src/modules/Navbar.tsx
+++ src/modules/Navbar.tsx
@@ -5,10 +5,10 @@ interface Props {}
 
 const Navbar: React.FC<Props> = () => (
   <nav id="navbar" className="navbar navbar-light bg-light fixed-top">
-    {/* <div className="container-fluid"> */}
+    <div className="container-fluid">
     <span className="navbar-brand mb-0 h1">Klassenstufen Teiler</span>
     <HelpDialog />
-    {/* </div> */}
+    </div>
   </nav>
 );
 
